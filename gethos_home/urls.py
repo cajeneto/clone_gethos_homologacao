a63b82b28a01cedb0066bf72212060a036203ddc
Hostílio Neto <netocajeh@gmail.com>
2024-10-08 16:52:27 -0300

    criação de arquivos .html e ajuste de layouts no html

--- gethos_home/urls.py
+++ gethos_home/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from django.http import HttpResponse
 
 #  importando da views do app
-from gethos_home.views import home, login_admin, login_user, register_menssage_user, console_status_menssage_user, register_contacts_user
+from gethos_home.views import home, create_account, dashboard_auth, login_admin, login_user, register_menssage_user, console_status_menssage_user, register_contacts_user
 
 
 urlpatterns = [
@@ -11,6 +11,8 @@ urlpatterns = [
     path('login_user/', login_user, name='login_user'),
     path('login_admin/', login_admin, name='login_admin'),
     path('', home, name='home'),
+    path('criarContaGethos', create_account, name='create_account'),
+    path('painelCentralUser/', dashboard_auth, name='dashboard_auth'),
     path('login_user/register_contacts_user/', register_contacts_user, name='register_contacts_user'),
     path('login_user/register_menssage_user/', register_menssage_user, name='login_user/register_menssage_user'),
     path('login_user/register_menssage_user/console_status_menssage_user', console_status_menssage_user, name='console_status_menssage_user'),
--- gethos_home/views.py
+++ gethos_home/views.py
@@ -10,6 +10,16 @@ def home(request):
 
 
 
+def create_account(request):
+    return render(request, 'create_account.html')
+
+
+
+def dashboard_auth(request):
+    return render(request, 'dashboard_auth.html')
+
+
+
 
 # configuração de url para login de usuário
 def login_user(request):
